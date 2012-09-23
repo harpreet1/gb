d60a55ad9dfa134082a491f7ee22e5e2ab70af49
Andras Kende <andras@kende.com>
2012-09-23 14:17:45 -0700

    adding Order OrderItem model, address validation, flash messages

---
+++ app/Model/Order.php
@@ -0,0 +1,122 @@
+<?php
+App::uses('AppModel', 'Model');
+class Order extends AppModel {
+
+//////////////////////////////////////////////////
+
+	public $validate = array(
+		'name' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				'message' => 'Name is invalid',
+				//'allowEmpty' => false,
+				'required' => true,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'email' => array(
+			'email' => array(
+				'rule' => array('email'),
+				'message' => 'Email is invalid',
+				//'allowEmpty' => false,
+				'required' => true,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'phone' => array(
+			'notempty' => array(
+				'rule' => array('phone'),
+				'message' => 'Phone is invalid',
+				'allowEmpty' => true,
+				'required' => true,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'billing_address' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				'message' => 'Billing Address is invalid',
+				//'allowEmpty' => false,
+				'required' => true,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'billing_city' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				'message' => 'Billing City is invalid',
+				//'allowEmpty' => false,
+				'required' => true,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'billing_state' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				'message' => 'Billing State is invalid',
+				//'allowEmpty' => false,
+				'required' => true,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'shipping_address' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				'message' => 'Shipping Address is invalid',
+				//'allowEmpty' => false,
+				'required' => true,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'shipping_city' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				'message' => 'Shipping City is invalid',
+				//'allowEmpty' => false,
+				'required' => true,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+		'shipping_state' => array(
+			'notempty' => array(
+				'rule' => array('notempty'),
+				'message' => 'Shipping State is invalid',
+				//'allowEmpty' => false,
+				'required' => true,
+				//'last' => false, // Stop validation after this rule
+				//'on' => 'create', // Limit validation to 'create' or 'update' operations
+			),
+		),
+
+
+	);
+
+//////////////////////////////////////////////////
+
+	public $hasMany = array(
+		'OrderItem' => array(
+			'className' => 'OrderItem',
+			'foreignKey' => 'order_id',
+			'dependent' => true,
+			'conditions' => '',
+			'fields' => '',
+			'order' => '',
+			'limit' => '',
+			'offset' => '',
+			'exclusive' => '',
+			'finderQuery' => '',
+			'counterQuery' => ''
+		)
+	);
+
+//////////////////////////////////////////////////
+
+}
