fb379f8aec2c3b0e7a21718a6e5aa4d0751c8e50
Andras Kende <andras@kende.com>
2012-09-23 04:05:44 -0700

    ups response formatting sorfting

--- app/Controller/Component/UpsComponent.php
+++ app/Controller/Component/UpsComponent.php
@@ -72,38 +72,38 @@ class UpsComponent extends Component {
 
 //////////////////////////////////////////////////
 
-	protected function formatresponse($response) {
+	protected function formatResponse($response) {
 
 		$service_code = array(
-			'US' => array(
-				'01' => 'UPS Next Day Air',
-				'02' => 'UPS 2nd Day Air',
-				'03' => 'UPS Ground',
-				'07' => 'UPS Worldwide Express',
-				'08' => 'UPS Worldwide Expedited',
-				'11' => 'UPS Standard',
-				'12' => 'UPS 3 Day Select',
-				'13' => 'UPS Next Day Air Saver',
-				'14' => 'UPS Next Day Air Early A.M.',
-				'54' => 'UPS Worldwide Express Plus',
-				'59' => 'UPS 2nd Day Air A.M.',
-			)
+			'01' => 'UPS Next Day Air',
+			'02' => 'UPS 2nd Day Air',
+			'03' => 'UPS Ground',
+			'07' => 'UPS Worldwide Express',
+			'08' => 'UPS Worldwide Expedited',
+			'11' => 'UPS Standard',
+			'12' => 'UPS 3 Day Select',
+			'13' => 'UPS Next Day Air Saver',
+			'14' => 'UPS Next Day Air Early A.M.',
+			'54' => 'UPS Worldwide Express Plus',
+			'59' => 'UPS 2nd Day Air A.M.',
 		);
 
-		$data = array();
+		$results = array();
 		$i = 0;
 
 		foreach($response['RatingServiceSelectionResponse']['RatedShipment'] as $result) {
 
 			$code = $result['Service']['Code'];
-			$data[$i]['ServiceCode'] = $code;
-			$data[$i]['ServiceName'] = $service_code['US'][$code];
-			$data[$i]['TotalCharges'] = $result['TotalCharges']['MonetaryValue'];
+			$results[$i]['ServiceCode'] = $code;
+			$results[$i]['ServiceName'] = $service_code[$code];
+			$results[$i]['TotalCharges'] = $result['TotalCharges']['MonetaryValue'];
 			$i++;
 
 		}
 
-		return $data;
+		$results = Set::sort($results, '{n}.TotalCharges', 'ASC');
+
+		return $results;
 
 	}
 
