d4f9f4d9d340abeb671e3ade6ef47bdf9baa4ec9
Andras Kende <andras@kende.com>
2012-10-12 17:55:04 -0700

    awning initial

--- app/webroot/awning.php
+++ app/webroot/awning.php
@@ -10,5 +10,24 @@ awning
 <br />
 <br />
 
-
 <img src="/img/awning-cont.png">
+
+<br />
+<br />
+<br />
+
+<div style="width: 680px; height: 120px; background-color: #aa0000"><img src="/img/awning.png"></div>
+
+<br />
+<br />
+
+<div style="width: 680px; height: 120px; background-color: #000099"><img src="/img/awning.png"></div>
+
+<br />
+<br />
+
+<div style="width: 680px; height: 120px; background-color: #006600"><img src="/img/awning.png"></div>
+
+<br />
+<br />
+
