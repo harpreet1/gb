ccac5b57858a2a5db63e8171f63c97af6eef4f6d
Eddie Reyes <er777@reyesworld.com>
2012-11-04 17:34:27 -0800

    Turned off debug

--- app/Controller/CategoriesController.php
+++ app/Controller/CategoriesController.php
@@ -53,7 +53,7 @@ class CategoriesController extends AppController {
 				'Subcategory.category_id' => $category['Category']['id']
 			)
 		));
-		debug($subcategories);
+		//debug($subcategories);
 		$this->set(compact('subcategories'));
 
 		$products = $this->Category->Product->find('all', array(
