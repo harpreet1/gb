81de0dbd56027e28d0f384ec067482bd3615bc0d
Eddie Reyes <er777@reyesworld.com>
2013-04-09 17:04:01 -0700

    More Homepage

--- app/webroot/js/homepage.js
+++ app/webroot/js/homepage.js
@@ -4,10 +4,8 @@ $(document).ready(function(){
 		$.vegas('slideshow', {
 			delay: 10000,
 			backgrounds: [
-				{ src: '/img/homepage/backgrounds/0.jpg', fade: 4000 },
-				{ src: '/img/homepage/backgrounds/1.jpg', fade: 4000 },
-				{ src: '/img/homepage/backgrounds/2.jpg', fade: 4000 },
-				{ src: '/img/homepage/backgrounds/3.jpg', fade: 4000 },
+				{ src: '/img/homepage/backgrounds/0.jpg'  },
+				
 			]
 		});
 //		$.vegas('overlay', {
