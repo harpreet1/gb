83aca1f809e5894d49ab9509035baa8d416c7b9b
Eddie Reyes <er777@reyesworld.com>
2013-06-18 15:59:15 -0700

    USPS Service Mod, Pic Scaler

--- app/Controller/Component/UspsComponent.php
+++ app/Controller/Component/UspsComponent.php
@@ -26,7 +26,7 @@ class UspsComponent extends Component {
 		App::uses('Xml', 'Utility');
 		$formattedResponse = Xml::toArray(Xml::build($response));
 
-		$serviceAllowed = array(0, 1, 3, 4);
+		$serviceAllowed = array(3, 4);
 
 		$results = array();
 		$i = 0;
